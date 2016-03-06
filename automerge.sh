9fe5b106598f17bf0de667e2697d48a8f0ccd32a
Jimmi Dyson <jimmidyson@gmail.com>
2016-03-06 21:13:40 +0000

    Fix automerge...

--- automerge.sh
+++ automerge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Auto-merging pull request ${CI_PULL_REQUEST}"
-STATUS_CODE=$(curl -qSfsw '\n%{http_code}' -XPUT -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -d"{\"sha\":\"${CIRCLE_SHA1}\"}" ${CI_PULL_REQUEST}/merge)
+STATUS_CODE=$(curl -qSfsw '\n%{http_code}' -XPUT -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -d"{\"sha\":\"${CIRCLE_SHA1}\"}" ${CI_PULL_REQUEST/\/pull\//\/pulls\/}/merge)
 echo "Received ${STATUS_CODE}"
 if [[ "$STATUS_CODE" -ne "200" ]]; then
   echo "Auto-merge failed..."
