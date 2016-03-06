4f36c27fb4c4d85eb77ca57d12c9d89f6b6f0a29
Jimmi Dyson <jimmidyson@gmail.com>
2016-03-06 21:41:27 +0000

    Fix merge URL

--- automerge.sh
+++ automerge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Auto-merging pull request ${CI_PULL_REQUEST}"
-STATUS_CODE=$(curl -qSfsw '\n%{http_code}' -XPUT -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -d"{\"sha\":\"${CIRCLE_SHA1}\"}" ${CI_PULL_REQUEST/\/pull\//\/pulls\/}/merge)
+MERGE_URL=${CI_PULL_REQUEST/\/pull\//\/pulls\/}/merge
+MERGE_URL=${MERGE_URL/github.com\//api.github.com\/repos\/}
+STATUS_CODE=$(curl -qSfsw '\n%{http_code}' -XPUT -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -d"{\"sha\":\"${CIRCLE_SHA1}\"}" -H 'Accept: application/vnd.github.v3+json' ${MERGE_URL})
 echo "Received ${STATUS_CODE}"
 if [[ "$STATUS_CODE" -ne "200" ]]; then
   echo "Auto-merge failed..."
