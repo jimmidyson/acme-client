3751ab878a3ed2d0f8b7d2bc7ac3967406d6faf6
Jimmi Dyson <jimmidyson@gmail.com>
2016-03-06 21:06:55 +0000

    Add automerge script

---
+++ automerge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Auto-merging pull request ${CI_PULL_REQUEST}"
+STATUS_CODE=$(curl -qSfsw '\n%{http_code}' -XPUT -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -d"{\"sha\":\"${CIRCLE_SHA1}\"}" ${CI_PULL_REQUEST})
+echo "Received ${STATUS_CODE}"
+if [[ "$STATUS_CODE" -eq "200" ]]; then
+  echo "Auto-merge failed..."
+  exit 1
+fi
+
